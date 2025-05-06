d0d0ae966c81081a2d24fa1d71fc04749e300710
Claymore-AI <dimalishchuk2003@gmail.com>
2025-05-06 22:05:31 +0300

    fix

--- cinema/views.py
+++ cinema/views.py
@@ -72,11 +72,11 @@ class MovieViewSet(
 
     @staticmethod
     def _params_to_ints(qs):
-        """Converts a list of string IDs to a list of integers"""
+        """Конвертує список ID в рядках у список цілих чисел"""
         return [int(str_id) for str_id in qs.split(",")]
 
     def get_queryset(self):
-        """Retrieve the movies with filters"""
+        """Отримати фільмів з фільтрами"""
         title = self.request.query_params.get("title")
         genres = self.request.query_params.get("genres")
         actors = self.request.query_params.get("actors")
